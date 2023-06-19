c9ad1a565ccc62abb94eae997709b87111b0e985
tgillain <57956193+tgillain@users.noreply.github.com>
2023-06-19 14:42:04 +0200

    Fix initiation problem

--- test-springboot/src/main/java/org/example/config/ConnectorProperties.java
+++ test-springboot/src/main/java/org/example/config/ConnectorProperties.java
@@ -1,19 +1,32 @@
 package org.example.config;
 
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import jakarta.annotation.PostConstruct;
+
+import static org.slf4j.LoggerFactory.*;
 
 @ConfigurationProperties
 public class ConnectorProperties {
 
+    private static final Logger LOGGER = getLogger(ConnectorProperties.class);
+
     @Value("${graceful-shutdown-period:60}")
     private long gracefulShutdownPeriod;
 
+    @Autowired
     private ConfigRepository configConfiguration;
 
     public ConnectorProperties(ConfigRepository configConfiguration) {
-        assert configConfiguration!=null : "Received null configuration";
         this.configConfiguration = configConfiguration;
+    }
+    
+    @PostConstruct
+    void init() {
+        LOGGER.info("Param value: {}",configConfiguration.getParam());
     }
     
     public String getParam() {
